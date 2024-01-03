0fa1264b6bca3fcee73deb43e4095c538e4844bb
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2024-01-03 10:38:28 +0530

    second commit

--- Goldman Sachs/combinationsumIII.cpp
+++ Goldman Sachs/combinationsumIII.cpp
@@ -1,34 +1,34 @@
 // Combination Sum III : Array , Backtracking
 
-class Solution {
-public:
-    void solve( vector<vector<int>> &ans , vector<int> &output , int i , int k , int n){
-        // Using recursion :
+// class Solution {
+// public:
+//     void solve( vector<vector<int>> &ans , vector<int> &output , int i , int k , int n){
+//         // Using recursion :
 
-        // Base Case : 
-        if(output.size() == k && n== 0){
-            ans.push_back(output);
-            return;
-        }
-        if(output.size() == k && n!= 0){
-            return;
-        }
+//         // Base Case : 
+//         if(output.size() == k && n== 0){
+//             ans.push_back(output);
+//             return;
+//         }
+//         if(output.size() == k && n!= 0){
+//             return;
+//         }
 
-        // Recursion : 
-        for(int j = i ; j<=9 ; j++){
-            output.push_back(j);
-            solve(ans , output , j+1 , k , n-j);
+//         // Recursion : 
+//         for(int j = i ; j<=9 ; j++){
+//             output.push_back(j);
+//             solve(ans , output , j+1 , k , n-j);
 
-            // Backtracking : 
-            output.pop_back();
-        }
-    }
-    vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
-        vector<int> output;
-        int i = 1;
-        solve(ans , output , i , k , n);
-        return ans;
-    }
-};
+//             // Backtracking : 
+//             output.pop_back();
+//         }
+//     }
+//     vector<vector<int>> combinationSum3(int k, int n) {
+//         vector<vector<int>> ans;
+//         vector<int> output;
+//         int i = 1;
+//         solve(ans , output , i , k , n);
+//         return ans;
+//     }
+// };
 
